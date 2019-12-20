c0d2d9704e18087f6fcaaecb9744d3cda3d06391
Mukesh Suman <sumanm686@gmail.com>
2019-12-20 18:29:19 +0530

    add producation build folder

---
+++ build/precache-manifest.bbadde3c4cbd36e19678764990fb91b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b2585b52904a1b589dd543e2502372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97e8457f2d463621a5b2",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "97e8457f2d463621a5b2",
+    "url": "/static/js/2.4cc75cb5.chunk.js"
+  },
+  {
+    "revision": "d2ccd9ff13c6ea26aa4933dbe6419a92",
+    "url": "/static/js/2.4cc75cb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06fd8c9480ada233623b",
+    "url": "/static/js/main.7e371ea6.chunk.js"
+  },
+  {
+    "revision": "5ea30fa3872f716a1ea8",
+    "url": "/static/js/runtime-main.803689d7.js"
+  },
+  {
+    "revision": "3d432ca2badb7e0130b379f9162b99b1",
+    "url": "/static/media/logo.3d432ca2.svg"
+  }
+]);
